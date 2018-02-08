079962dd063ea265636cae208d2e23733baccf23
Patrick Lu <lightning.lu7@gmail.com>
2018-02-08 14:27:30 -0800

    updated readonly as transparent bg

--- src/Block.js
+++ src/Block.js
@@ -167,7 +167,7 @@ export default class Block extends Component {
                   rows={1}
                   disabled={readOnly}
                   placeholder={this.state.placeholder}
-                  className={css(styles.input)}
+                  className={css(styles.input, readOnly && styles.readOnlyInput)}
                   onChange={this._handleCaptionChange}
                   value={this.props.data.caption}
                 />
@@ -185,6 +185,9 @@ var styles = StyleSheet.create({
     textAlign: 'center',
 
   },
+  readOnlyInput: {
+    background: 'transparent',
+  },
   input: {
     border: 'none',
     outline: 'none',
@@ -195,6 +198,7 @@ var styles = StyleSheet.create({
     width: '100%',
     textAlign: 'center',
     resize: 'none',
+    boxSizing: 'border-box',
   },
   videoDiv: {
     position: 'relative',
